fae90bb76f30c97284874262b989a381faf968fb
Muhammad Abdurrahman <muhammad.abdurrahman.uk@gmail.com>
2016-11-06 19:53:37 +0000

    extract margin definitions

--- parser.rb
+++ parser.rb
@@ -1,3 +1,15 @@
 require 'nokogiri'
 
-hans_wehr_source = File.open("hanswehr.xml") { |f| Nokogiri::XML(f) }
+hw_styles = File.open("hanswehr_styles.xml") { |f| Nokogiri::XML(f)}
+hw_source = File.open("hanswehr_content_only.xml") { |f| Nokogiri::XML(f) }
+
+margins = Hash.new {|h,k| h[k]=[]}
+
+hw_styles.xpath("//style:paragraph-properties[@fo:margin-left]").each do |m|
+    margins[m["fo:margin-left"]] = m.parent["style:name"]
+end
+
+# f_out = File.new("out.txt", "w+")
+
+# f_out.write hw_source.xpath("//text:p[@text:style-name='P15']").first
+# f_out.close
