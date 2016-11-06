1f51dff0502f3cca7c2b661343e8c05b9e551250
Muhammad Abdurrahman <muhammad.abdurrahman.uk@gmail.com>
2016-11-06 20:58:14 +0000

    refactor margins to styles

--- parser.rb
+++ parser.rb
@@ -4,13 +4,13 @@ require 'json'
 hw_styles = File.open("hanswehr_styles.xml") { |f| Nokogiri::XML(f)}
 hw_source = File.open("hanswehr_content_only.xml") { |f| Nokogiri::XML(f) }
 
-margins = Hash.new {|h,k| h[k]=[]}
+styles = Hash.new {|h,k| h[k]=[]}
 
-hw_styles.xpath("//style:paragraph-properties[@fo:margin-left]").each do |m|
-    margins["ml="+m["fo:margin-left"]+"_ti="+m["fo:text-indent"]] << m.parent["style:name"]
+hw_styles.xpath("//style:paragraph-properties[@fo:margin-left]").each do |s|
+    styles["ml="+s["fo:margin-left"]+"_ti="+s["fo:text-indent"]] << s.parent["style:name"]
 end
 
-puts JSON.pretty_generate(margins).gsub(":", " =>")
+puts JSON.pretty_generate(styles).gsub(":", " =>")
 
 # f_out = File.new("out.txt", "w+")
 
