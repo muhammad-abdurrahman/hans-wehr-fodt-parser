b1bf12b09efd34cca07c9acb78b6db40ea19d7fa
Muhammad Abdurrahman <muhammad.abdurrahman.uk@gmail.com>
2016-11-06 20:54:47 +0000

    add text-indent value in margin key

--- parser.rb
+++ parser.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'json'
 
 hw_styles = File.open("hanswehr_styles.xml") { |f| Nokogiri::XML(f)}
 hw_source = File.open("hanswehr_content_only.xml") { |f| Nokogiri::XML(f) }
@@ -6,8 +7,10 @@ hw_source = File.open("hanswehr_content_only.xml") { |f| Nokogiri::XML(f) }
 margins = Hash.new {|h,k| h[k]=[]}
 
 hw_styles.xpath("//style:paragraph-properties[@fo:margin-left]").each do |m|
-    margins[m["fo:margin-left"]] << m.parent["style:name"]
+    margins["ml="+m["fo:margin-left"]+"_ti="+m["fo:text-indent"]] << m.parent["style:name"]
 end
+
+puts JSON.pretty_generate(margins).gsub(":", " =>")
 
 # f_out = File.new("out.txt", "w+")
 
