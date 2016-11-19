20f0565ebc7f75507da86c0cc5aaa0fef6988e29
Muhammad Abdurrahman <muhammad.abdurrahman.uk@gmail.com>
2016-11-19 22:35:14 +0000

    improve code formatting

--- parser.rb
+++ parser.rb
@@ -6,7 +6,7 @@ hw_source = File.open("hanswehr.xml") { |f| Nokogiri::XML(f) }
 styles = Hash.new {|h,k| h[k]=[]}
 
 hw_source.xpath("//style:paragraph-properties[@fo:margin-left]").each do |s|
-    styles["#{s["fo:margin-left"].tr('in','').to_f + s["fo:text-indent"].tr('in','').to_f}"] << s.parent["style:name"]
+    styles["#{s["fo:margin-left"].delete('in').to_f + s["fo:text-indent"].delete('in').to_f}"] << s.parent["style:name"]
 end
 
 Pry::ColorPrinter.pp(styles.sort_by{|k,v| k}.to_h)
