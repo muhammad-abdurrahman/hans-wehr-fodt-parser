268e6c8366cea18b92e6756b05a99d45803afd28
jamal.osman <jamal.osman@cohaesus.co.uk>
2016-12-23 11:07:16 +0000

    extract words into a hash, and attempt to identify root words based on the style-name
    It seemes that the document doesn't always conform to the rule that root words have no indent and sub words are intented in from the root word, doesn't look like we can rely on this.

--- parser.rb
+++ parser.rb
@@ -1,5 +1,9 @@
 require 'nokogiri'
-require 'pry'
+# require 'pry'
+
+# # check unicode values for arabic characters (between 1571 and 1618)
+# ara_chars = "ابتثجحخدذرزسشصضطظعغفقكلمنهويأإىؤءئًٌٍَُِّْ"
+# ara_chars.chars.each { |char| puts char + ": " + char.ord.to_s } 
 
 hw_source = File.open("hanswehr.xml") { |f| Nokogiri::XML(f) }
 
@@ -9,7 +13,26 @@ hw_source.xpath("//style:paragraph-properties[@fo:margin-left]").each do |s|
     styles["#{s["fo:margin-left"].delete('in').to_f + s["fo:text-indent"].delete('in').to_f}"] << s.parent["style:name"]
 end
 
-Pry::ColorPrinter.pp(styles.sort_by{|k,v| k}.to_h)
+p "alif hamza: " "أ".ord
+
+root_word_styles = styles["0.0"]
+
+regex = /(?<= |^)[\u0620-\u0660 ]+(?= |$)/
+
+root_words = hw_source.xpath("//office:text/text:p")
+	.map{ |tag| 
+            { 
+                word: regex.match(tag.text).to_s, 
+                text: tag.text, 
+                style: tag.attributes["style-name"].value, 
+                is_root: root_word_styles.include?(tag.attributes["style-name"].value)
+            }
+        } 
+
+root_words.each { |rw| puts rw };
+
+
+# Pry::ColorPrinter.pp(styles.sort_by{|k,v| k}.to_h)
 
 # f_out = File.new("out.txt", "w+")
 
