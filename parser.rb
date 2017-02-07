2979db8251c16deeb602eea573a130dd0575f2df
Jamal Osman <jamal.osman93@gmail.com>
2017-02-07 23:21:23 +0000

    added new regex to detect root word

--- parser.rb
+++ parser.rb
@@ -14,34 +14,38 @@ hw_source.xpath("//style:paragraph-properties[@fo:margin-left]").each do |s|
     styles["#{s["fo:margin-left"].delete('in').to_f + s["fo:text-indent"].delete('in').to_f}"] << s.parent["style:name"]
 end
 
-p "alif hamza: " "أ".ord
 
-root_word_styles = styles["0.0"]
+$root_word_styles = styles["0.0"]
 
 regex = /(?<= |^)[\u0620-\u0660 ]+(?= |$)/
 current_root = nil;
 autonum = 1
 
+def check_is_root(tag)
+	styleMatch = $root_word_styles.include? tag.attributes["style-name"].value
+	# matches a lot of root words based on fitting the expression "فعل fa'ala a" with some extra checks
+	# has to be directly at the beginning of the definition
+	rootBeginningRegex = /^\d?[\u0620-\u0660]{3} *[a-z']+ *[aiu] / 
+	return styleMatch
+end
+
 root_words = hw_source.xpath("//office:text/text:p")
 	.map{ |tag| 
-            is_root = check_is_root(tag)
+            
             word = { 
             	id: autonum,
                 word: regex.match(tag.text).to_s, 
                 text: tag.text, 
-                is_root: is_root,
+                is_root: check_is_root(tag),
                 root: current_root
             }
-            current_root = autonum if is_root
+            current_root = autonum if check_is_root(tag)
             autonum += 1 
            	word
         } 
 
-File.write 'results.json', root_words[0...1000].to_json
+File.write 'results.json', root_words[0...10000].to_json
 
-def check_is_root(tag) {
-	root_word_styles.include?(tag.attributes["style-name"].value)
-}
 
 
 # Pry::ColorPrinter.pp(styles.sort_by{|k,v| k}.to_h)
